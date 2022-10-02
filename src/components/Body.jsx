c2fc0a894618d00c08f2dbb85393b84d96e9b60e
anikman <anikman98@gmail.com>
2022-10-02 23:28:03 +0530

    added spotify icon and logo and fixed base layout with CSS

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,13 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Body = () => {
+
+    let d = new Date();
+
   return (
-    <Container>Body</Container>
+    <Container>
+        <div className="greetings">
+            {
+                (d.getHours() >=0 && d.getHours < 12) 
+                ? "Good morning" 
+                : ((d.getHours() >=12 && d.getHours < 17) 
+                ? "Good afternoon" 
+                : "Good evening"
+                ) 
+             }
+        </div>
+    </Container>
   )
 }
 
 const Container = styled.div`
     overflow: auto;
+    padding-top: 0.2rem;
+    padding-left: 0.7rem;
+    padding-right: 0.7rem;
+    font-weight: 600;
+
+    .greetings{
+        font-size: 2rem;
+    }
 `;
 
 export default Body
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import Playlist from './Playlist'
 
 const Sidebar = () => {
   return (
-    <Container>Sidebar</Container>
+    <Container>
+        <div className="library">
+            Library
+        </div>
+        <hr />
+        <div className="playlist">
+            <Playlist />
+        </div>
+    </Container>
   )
 }
 
 const Container = styled.div`
     height: 100%;
+    padding: 0 0.7rem;
+    
 `;
 
 export default Sidebar
